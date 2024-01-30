eef86cd052a4e5f566a4e8be2085e5ec455831ae
Umam Khan <umam.khan@outlook.com>
2024-01-30 22:34:04 +0530

    folder structure created

---
+++ frontend/gen-ai/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  return (
+    <>
+      <h1 className='underline font-bold bg-red-50'>hello worldddd </h1>
+    </>
+  )
+}
+
+export default App
